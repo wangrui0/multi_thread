ad548def22a4f0d84e4307b4ad04b45214d8bdc5
wangrui <1176306419@qq.com>
2018-09-08 09:16:57 +0800

    并发

---
+++ src/main/java/main/itcast/heima/LockTest.java
@@ -0,0 +1,63 @@
+package main.itcast.heima;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * jdk1.5使用new ReentrantLock()进行同步；注意unlock要放在finally中
+ * 注意：锁要锁住资源，而不要锁线程；
+ * 
+ * @author jack
+ *
+ */
+public class LockTest {
+	public static void main(String[] args) {
+		Output output = new Output();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(1);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					output.output("zhangxiaoxiang");
+				}
+			}
+		}).start();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(1);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					output.output("wangrui");
+				}
+			}
+		}).start();
+	}
+
+	/**
+	 * 打印 字母的 方法
+	 */
+	static class Output {
+		Lock lock = new ReentrantLock();
+		String c = "";
+		public void output(String name) {
+			lock.lock();
+			try {
+				int len = name.length();
+				for (int i = 0; i < len; i++) {
+					System.out.print(name.charAt(i));
+				}
+				System.out.println();
+			} finally {
+				lock.unlock();
+			}
+		}
+	}
+}
---
+++ src/main/java/main/itcast/heima/ThreadScopeShareDataThreadLocal2.java
@@ -0,0 +1,65 @@
+package main.itcast.heima;
+
+import java.util.Random;
+
+/**
+ * 线程范围内的共享数据 
+ * 使用单例设计模式 进行共享 数据；
+ * @author jack
+ *
+ */
+public class ThreadScopeShareDataThreadLocal2 {
+	private static ThreadLocal<Integer> threadLocal=new ThreadLocal<Integer>();
+	public static void main(String[] args) {
+		for(int i=0;i<3;i++){
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					Integer num= new Random().nextInt(20000);
+					ShareDataBean.getInstance().setName(String.valueOf(num));
+					ShareDataBean.getInstance().setPwd(String.valueOf(num));
+					System.out.println(Thread.currentThread().getName()+"==="+new A().getA());
+					System.out.println(Thread.currentThread().getName()+"==="+new B().getB());
+				}
+			}).start();
+		}
+	}
+	
+	static class A{
+		public Integer getA(){
+			return Integer.valueOf(ShareDataBean.getInstance().getName());
+		}
+	}
+	static class B{
+		public  Integer getB(){
+			return Integer.valueOf(ShareDataBean.getInstance().getPwd());
+		}
+	}
+}
+class ShareDataBean{
+	private static ThreadLocal<ShareDataBean> threadLocal=new ThreadLocal<ShareDataBean>();
+	private ShareDataBean(){
+	}
+	public static ShareDataBean getInstance(){
+		ShareDataBean shareDataBean= threadLocal.get();
+		if(shareDataBean==null){
+			shareDataBean=new ShareDataBean();
+			threadLocal.set(shareDataBean);
+		}
+		return  shareDataBean;
+	}
+	private String name;
+	private String pwd;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+}
